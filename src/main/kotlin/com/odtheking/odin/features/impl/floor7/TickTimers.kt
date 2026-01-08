311498e718f482f75f0371a970434c4d451945f4
thomjap55 <thomjap55@gmail.com>
2026-01-08 22:02:30 +0100

    Update TickTimers.kt

--- src/main/kotlin/com/odtheking/odin/features/impl/floor7/TickTimers.kt
+++ src/main/kotlin/com/odtheking/odin/features/impl/floor7/TickTimers.kt
@@ -119,7 +119,7 @@ object TickTimers : Module(
      
         onReceive<ClientboundSetTimePacket> {
             if (!DungeonUtils.inClear) return@onReceive
-            if (outboundsHud.enabled && DungeonUtils.inBoss) outboundsTime = 40 - (gameTime % 40).toInt()
+            if (DungeonUtils.inBoss && outboundsHud.enabled) outboundsTime = 40 - (gameTime % 40).toInt()
             if (DungeonUtils.openRoomCount == 0) {
             } else {
                 if (secretsHud.enabled) secretsTime = 20 - (gameTime % 20).toInt()
